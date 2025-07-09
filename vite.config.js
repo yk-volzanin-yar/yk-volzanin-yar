becd00ba68957027081cfa4f9f7a9a17f7145f18
ElizabethGlebezdova <elizabeth.glebezdova@yandex.ru>
2025-07-09 22:08:22 +0300

    fix base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/yk-volzanin-yar/',
   plugins: [react()],
   resolve: {
     alias: {
